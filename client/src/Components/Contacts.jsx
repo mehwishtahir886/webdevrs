f3e3c6b2fada4063dbe5eea6fe4c73454c96382c
unknown <awaisweb4@gmail.com>
2022-06-07 13:29:03 +0500

    add section in footer

--- client/src/Components/Contacts.jsx
+++ client/src/Components/Contacts.jsx
@@ -5,8 +5,8 @@ import "./Contact.module.css"
 import classes from "./Contact.module.css"
 import "./Navbar.css"
 import Button from 'react-bootstrap/Button'
-import { BsFacebook,BsTwitter,BsInstagram,BsGoogle} from "react-icons/bs";
-import {AiOutlineMail,AiOutlineWhatsApp,AiOutlineMobile} from "react-icons/ai";
+import { BsFacebook, BsTwitter, BsInstagram, BsGoogle } from "react-icons/bs";
+import { AiOutlineMail, AiOutlineWhatsApp, AiOutlineMobile } from "react-icons/ai";
 import { GoLocation } from "react-icons/go";
 
 
@@ -22,31 +22,31 @@ const Contacts = () => {
               <Col className={classes.Detail_contact} xl={5} lg={5} md={5} sm={5} >
                 <h3 className={classes.Contact_Name}>Contact Us</h3>
                 <div className={classes.Detail_box}>
-                  <GoLocation className={classes.Location_icon}/>
+                  <GoLocation className={classes.Location_icon} />
                   <p className={classes.Details_li}>Main Gulberg-|||,Halli Road,IT Tower,Lahore</p>
                 </div>
                 <div className={classes.lis_contact}>
-                  <AiOutlineMail className={classes.Location_icon}/>
+                  <AiOutlineMail className={classes.Location_icon} />
                   <p className={classes.Details_li}>Webdevrshub@gmail.com</p>
                 </div>
                 <div className={classes.lis_contact}>
-                  <AiOutlineWhatsApp className={classes.Location_icon}/>
+                  <AiOutlineWhatsApp className={classes.Location_icon} />
                   <p className={classes.Details_li}>+92 3021446562</p>
                 </div>
                 <div className={classes.lis_contact}>
-                  <AiOutlineMobile className={classes.Location_icon}/>
+                  <AiOutlineMobile className={classes.Location_icon} />
                   <p className={classes.Details_li}>+92 3056520720</p>
                 </div>
                 <div className={classes.SocialContact}>
-                  <BsFacebook className={classes.cont_Social}/>
-                    <BsTwitter className={classes.cont_Social} />
-                    <BsInstagram className={classes.cont_Social}/>
-                    <BsGoogle className={classes.cont_Social}/>
+                  <BsFacebook className={classes.cont_Social} />
+                  <BsTwitter className={classes.cont_Social} />
+                  <BsInstagram className={classes.cont_Social} />
+                  <BsGoogle className={classes.cont_Social} />
                 </div>
               </Col>
               <Col className={classes.Form_box} xl={7} lg={6} md={6} sm={6}>
-                  <h4 className={classes.getTouch}>Get in Touch</h4>
-                  <p className={classes.FeelFree}>Feel free to drop us lines below</p>
+                <h4 className={classes.getTouch}>Get in Touch</h4>
+                <p className={classes.FeelFree}>Feel free to drop us lines below</p>
                 <Form>
                   <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                     <Form.Label className={classes.LabelName}>Name</Form.Label>
--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import classes from "./Footer.module.css"
+import { Container, Col, Row } from "react-bootstrap"
+import FoooterLogo from "../images/logo.png"
+import { BsFacebook, BsTwitter, BsInstagram, BsGoogle } from "react-icons/bs";
+
+
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+    return (
+        <>
+            <Container fluid className={classes.MainFooter} >
+                <footer>
+                    <Row>
+                        <Col className={classes.Box} xl={4}>
+                            <div className={classes.Footer_logo}>
+                                {/* <img className={classes.footer_img} src={FoooterLogo} /> */}
+                                <h2 style={{ color: "var(--primary-light)" }}>Web<span style={{ color: "rgb(248,34,73)" }}>Devrs</span></h2>
+                            </div>
+                            <p className={classes.followPara}>Lorem ipsum dolor sit amet, consec tetur adipisicing elit, sed do eiusmod tempor incididuntut consec tetur adipisicing elit,Lorem ipsum dolor sit amet..</p>
+                            <h3 className={classes.Folllow}>Follow Us</h3>
+                            <div className={classes.FootersocialIcons}>
+                                <div className={classes.Footer_Socialicon}>
+                                    <BsFacebook />
+                                </div>
+                                <div className={classes.Footer_Socialicon}>
+                                    <BsTwitter />
+                                </div>
+                                <div className={classes.Footer_Socialicon}>
+                                    <BsGoogle />
+                                </div>
+                            </div>
+                        </Col>
+                        <Col className={classes.Box} xl={2}>
+                        </Col>
+                        <Col className={classes.Box} xl={2}>
+                        </Col>
+                        <Col className={classes.Box} xl={2}>
+                        </Col>
+                    </Row>
+                </footer>
+            </Container>
+        </>
+    )
 }
 
 export default Footer
