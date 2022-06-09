009811a31a20b44a0c2640b6bb63beeac365b3ed
unknown <awaisweb4@gmail.com>
2022-06-09 13:53:19 +0500

    Add Signup Form

--- client/src/Components/SignUp.jsx
+++ client/src/Components/SignUp.jsx
@@ -1,15 +1,44 @@
 import React from 'react'
-import { Container } from 'react-bootstrap';
+import { Container, Form, Button } from 'react-bootstrap';
 import classes from "./SignUp.module.css"
 const SignUp = () => {
-  return (
-    <>
-    <Container fluid className={classes.Main_Signup}>
-        <div className={classes.SignUpoverlay}></div>
-        <div className={classes.Signupform}></div>
-    </Container>
-    </>
-  )
+    return (
+        <>
+            <Container fluid className={classes.Main_Signup}>
+                <div className={classes.SignUpoverlay}></div>
+                <div className={classes.Signupform}>
+                    <div className={classes.AlertDiv}>
+                        Sign Up
+                    </div>
+                    <Form className={classes.SignUpFormm}>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label className={classes.Label}>Name</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Name" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label className={classes.Label}>Last Name</Form.Label>
+                            <Form.Control type="email" placeholder="Enter Last Name" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label className={classes.Label}>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label className={classes.Label}>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label className={classes.Label}>Confirm-Password</Form.Label>
+                            <Form.Control type="password" placeholder="Confirm-Password" />
+                        </Form.Group>
+                        <Button className={classes.Upbtn} variant="primary" type="submit">
+                            Signup
+                        </Button>
+                    </Form>
+                </div>
+            </Container>
+        </>
+    )
 }
 
 export default SignUp
