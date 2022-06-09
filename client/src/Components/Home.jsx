08f09dcca24d083b94d363bb2cdc289279ade7bc
unknown <expertdeveloper8888>
2022-06-09 08:54:09 -0700

    m

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -7,10 +7,11 @@ import Navbar from './Navbar'
 import Roadmap from './Roadmap'
 import Services from './Services'
 import SignUp from './SignUp'
+import SignUp from './Homeabout'
 const Home = () => {
   return (
     <div>
-    
+        <Homeabout/>
         <Navbar/>
         <Roadmap/>
         <Courses />
