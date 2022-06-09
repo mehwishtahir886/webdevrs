d608a19f6cdde7ad9f1d9cfa7cfbd71b3e81c868
unknown <awaisweb4@gmail.com>
2022-06-09 17:39:10 +0500

    Change Btn Styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Route, Routes} from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 import ContactsPage from './Components/ContactsPage'
 import Home from './Components/Home'
 import Login from './Components/Login'
@@ -8,16 +8,16 @@ import SignUp from './Components/SignUp'
 
 const App = () => {
   return (
-    
+
     <>
-    
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/projects" element={<Projects />}/>
-      <Route path="//ContactPage" element={<ContactsPage/>}/>
-      <Route path='/login' element={<Login/>} />
-      <Route path='/Signup' element={<SignUp/>}/>
-    </Routes>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="//ContactPage" element={<ContactsPage />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/Signup' element={<SignUp />} />
+      </Routes>
     </>
   )
 }
