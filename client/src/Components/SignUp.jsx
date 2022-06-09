bed147810d84472b6f06ffa57233e20b369754d9
unknown <awaisweb4@gmail.com>
2022-06-09 15:25:45 +0500

    Changes

--- client/src/Components/SignUp.jsx
+++ client/src/Components/SignUp.jsx
@@ -10,37 +10,36 @@ const SignUp = () => {
                 <div className={classes.SignUpoverlay}></div>
                 <div className={classes.Signupform}>
                     <div className={classes.AlertDiv}>
-                        Sign Up
+                        <h3 className={classes.Uphead}>Sign Up</h3>
                     </div>
                     <Form className={classes.SignUpFormm}>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label className={classes.Label}>Name</Form.Label>
-                            <Form.Control type="text" placeholder="Enter Name" />
+                            <Form.Control className={classes.Plachold} type="text" placeholder="Enter Name" />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label className={classes.Label}>Last Name</Form.Label>
-                            <Form.Control type="email" placeholder="Enter Last Name" />
+                            <Form.Control className={classes.Plachold} type="email" placeholder="Enter Last Name" />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label className={classes.Label}>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Control className={classes.Plachold} type="email" placeholder="Enter email" />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label className={classes.Label}>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
+                            <Form.Control className={classes.Plachold} type="password" placeholder="Password" />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label className={classes.Label}>Confirm-Password</Form.Label>
-                            <Form.Control type="password" placeholder="Confirm-Password" />
+                            <Form.Control className={classes.Plachold} type="password" placeholder="Confirm-Password" />
                         </Form.Group>
                         <Button className={classes.Upbtn} variant="primary" type="submit">
                             Signup
                         </Button>
                         <p className={classes.Already}> Already an Account ? &nbsp; 
-                            <Link to="/login">
-                            <span>Click Here</span> 
+                            <Link style={{textDecoration:"none"}} to="/login">
+                            <span style={{color:"var(--primary-light)"}}>Click Here</span> 
                             </Link>
-                        
                         </p>
                         
                     </Form>
