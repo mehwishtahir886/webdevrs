07767cb5567ba0afd59d4c7fea135432a39cb8dd
unknown <awaisweb4@gmail.com>
2022-06-09 12:10:00 +0500

    ch

--- client/src/Components/SignUp.jsx
+++ client/src/Components/SignUp.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import classes from "./SignUp.module.css"
 const SignUp = () => {
   return (
