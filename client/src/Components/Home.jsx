763fd3d771e14c61556215ba9a3721c7fe357b83
unknown <expertdeveloper8888>
2022-06-09 04:38:31 -0700

    roadmap

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import About from './About'
-
 import Courses from './Courses'
-
 import Footer from './Footer'
 import Navbar from './Navbar'
+import Roadmap from './Roadmap'
 import Services from './Services'
 import SignUp from './SignUp'
 const Home = () => {
   return (
     <div>
+      
         <Navbar/>
         <About/>
         <SignUp/>
+        <Roadmap/>
         <Courses />
         <Services/>
         <Footer/>
---
+++ client/src/Components/Roadmap.jsx
@@ -0,0 +1,129 @@
+
+import React from 'react'
+import classes from './Roadmap.module.css';
+import {
+    Container,
+    Row,
+    Col,
+} from 'react-bootstrap';
+
+
+const Roadmap = () => {
+    return (
+        <>
+
+                  <Container className={classes.con}>
+
+                  
+
+            {/* row 1 */}
+
+            <Row className={classes.row} >
+
+
+                <div className={classes.imgp} >
+
+                    <img className={classes.img} src="https://images.unsplash.com/photo-1452457750107-cd084dce177d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y29tcHV0ZXIlMjBwcm9ncmFtZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+
+                </div>
+
+
+                <div className={classes.pa}>
+                    <h5>Step 1 :</h5>
+                    <h1>Chose A Programme .</h1>
+                    <p>
+                    Partner with the addmission rep to talk about your carrier goals 
+                        and chose the right programme with you , then chose form range
+                        of students-freindly payment options such paying upforts , month
+                        Partner with the addmission rep to talk about your carrier goals 
+                        and chose the right programme with you , then chose form range
+                        of students-freindly payment options such paying upforts , month
+                        Partner with the addmission rep to talk about your carrier goals 
+                        and chose the right programme with you , then chose form range
+                        of students-freindly payment options such paying upforts , month
+                        Partner with the addmission rep to talk about your carrier goals 
+                        to month or deferring tution until after you are hire
+                    </p>
+                </div>
+
+            </Row>
+
+
+
+            {/* //////////////////////////////////////////// */}
+
+            {/* row 2 */}
+
+            <Row className={classes.row} >
+
+                <div className={classes.pa}>
+                    <h5>Step 2 :</h5>
+                    <h1>Start Learning .</h1>
+                    <p>
+                    Partner with the addmission rep to talk about your carrier goals 
+                        and chose the right programme with you , then chose form range
+                        of students-freindly payment options such paying upforts , month
+                        Partner with the addmission rep to talk about your carrier goals 
+                        and chose the right programme with you , then chose form range
+                        of students-freindly payment options such paying upforts , month
+                        Partner with the addmission rep to talk about your carrier goals 
+                        and chose the right programme with you , then chose form range
+                        of students-freindly payment options such paying upforts , month
+                        Partner with the addmission rep to talk about your carrier goals 
+                        to month or deferring tution until after you are hire
+                    </p>
+                </div>
+
+                <div className={classes.imgp} >
+
+                    <img className={classes.img} src="https://images.unsplash.com/photo-1452457750107-cd084dce177d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y29tcHV0ZXIlMjBwcm9ncmFtZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+
+                </div>
+
+
+            </Row>
+
+            {/* /3/ */}
+
+
+            <Row className={classes.row} >
+
+
+                <div className={classes.imgp} >
+
+                    <img className={classes.img} src="https://images.unsplash.com/photo-1452457750107-cd084dce177d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y29tcHV0ZXIlMjBwcm9ncmFtZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+
+                </div>
+
+
+                <div className={classes.pa}>
+                    <h5>Step 3 :</h5>
+                    <h1>Get Hired .</h1>
+                    <p>
+                        Partner with the addmission rep to talk about your carrier goals 
+                        and chose the right programme with you , then chose form range
+                        of students-freindly payment options such paying upforts , month
+                        Partner with the addmission rep to talk about your carrier goals 
+                        and chose the right programme with you , then chose form range
+                        of students-freindly payment options such paying upforts , month
+                        Partner with the addmission rep to talk about your carrier goals 
+                        and chose the right programme with you , then chose form range
+                        of students-freindly payment options such paying upforts , month
+                        Partner with the addmission rep to talk about your carrier goals 
+                        to month or deferring tution until after you are hire
+                    </p>
+                </div>
+
+            </Row>
+
+            </Container>
+
+            {/* ////////////////// */}
+
+
+
+        </>
+    )
+}
+
+export default Roadmap
