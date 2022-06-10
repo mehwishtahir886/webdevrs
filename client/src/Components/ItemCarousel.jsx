686f09172d81c89ad997cb9c350c38d08cb95a4f
unknown <awaisweb4@gmail.com>
2022-06-10 15:21:12 +0500

    Carousel Ready

--- client/src/Components/ItemCarousel.jsx
+++ client/src/Components/ItemCarousel.jsx
@@ -8,7 +8,7 @@ export default styled.div`
   width: 100%;
   // background-color: var(--primary-dark);
   // color: #fff;
-  // margin: 15px;
+  margin: 15px;
   // font-size: 4em;
   border:1px solid;
 `;
