e1c32b12f7871d27dfc7b39a5c444b6391a879ed
unknown <awaisweb4@gmail.com>
2022-06-11 12:19:03 +0500

    Add Card

--- client/src/Components/Carouselslider.jsx
+++ client/src/Components/Carouselslider.jsx
@@ -2,22 +2,38 @@ import React, { useState } from "react";
 import Carousel from "react-elastic-carousel";
 import Item from "./ItemCarousel";
 import classes from "./Carousel.module.css"
+import CardImage from "../images/Laptop.png"
 
 const breakPoints = [
     { width: 1, itemsToShow: 1 },
     { width: 550, itemsToShow: 2 },
-    { width: 768, itemsToShow: 3 ,},
+    { width: 768, itemsToShow: 3, },
 ];
 
 const Carouselslider = () => {
-    const [items, setItems] = useState([1, 2, 3, 4 ,5,6,]);
+    const [items, setItems] = useState([1, 2, 3, 4, 5, 6,]);
     return (
         <>
             <div className={classes.App}>
                 <div className={classes.carousel_wrapper}>
                     <Carousel breakPoints={breakPoints}>
                         {items.map((item) => (
-                            <Item >{item}</Item>
+                            <Item >
+                                <div className={classes.MianCard}>
+                                    <div className={classes.cardHead}>Independent Project</div>
+                                    <div className={classes.cardImage}>
+                                        <img className={classes.cardimg} src={CardImage} />
+                                    </div>
+                                    <h5 className={classes.CardHeading} style={{ textAlign: "center" }}>Experience build building <br /> a Google News like App </h5>
+                                    <p className={classes.CardPara}>Build Highly Responsive Websites <br /> Fronteend For App</p>
+                                    <div className={classes.CardLanguage}>
+                                        <div className={classes.Lang1}>HTML</div>
+                                        <div className={classes.Lang1}>CSS</div>
+                                        <div className={classes.Lang1}>Javascript</div>
+                                        <div className={classes.Lang1}>Rect.js</div>
+                                    </div>
+                                </div>
+                            </Item>
                         ))}
                     </Carousel>
                 </div>
--- client/src/Components/ItemCarousel.jsx
+++ client/src/Components/ItemCarousel.jsx
@@ -4,11 +4,12 @@ export default styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  height:450px;
+  <height:500></height:500>px;
   width: 100%;
-  // background-color: var(--primary-dark);
+  background-color: var(--primary-light);
   // color: #fff;
-  margin: 15px;
+  border-radius:10px ;
+  margin: 35px;
   // font-size: 4em;
   border:1px solid;
 `;
