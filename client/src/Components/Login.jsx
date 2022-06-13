3ce2eb6bdd2a02f528f01faf6fd969374874678f
unknown <awaisweb4@gmail.com>
2022-06-13 15:32:45 +0500

    Login Changes

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
                     <Form className={classes.LoginFormm}>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label className={classes.loginLabel}>Email address</Form.Label>
-                            <Form.Control className={classes.LoginPlace} type="email" placeholder="&f218;Enter email" />
+                            <Form.Control className={classes.LoginPlace} type="email" placeholder="Enter email" />
                             <Form.Text className="text-muted">
                                 We'll never share your email with anyone else.
                             </Form.Text>
