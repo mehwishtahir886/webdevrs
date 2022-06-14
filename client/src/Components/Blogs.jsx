3bcf9e506fa95ad6bff17405bd71673a082ec278
unknown <awaisweb4@gmail.com>
2022-06-14 16:00:49 +0500

    Blogs Section Complete

--- client/src/Components/Blogs.jsx
+++ client/src/Components/Blogs.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Container,Row} from "react-bootstrap"
+import { Container, Row } from "react-bootstrap"
 import classes from "./Blogs.module.css"
 import Navbar from "./Navbar"
 import "./Navbar.css"
@@ -8,36 +8,48 @@ const Blogs = () => {
     console.log('====================================');
     console.log(BlogsData);
     console.log('====================================');
-    const BlogsMap = BlogsData.map((item)=>{
-        return(
+    const BlogsMap = BlogsData.map((item) => {
+        return (
             <>
-            <Row>
-                <div className={classes.BlogCard}>
-                    <div className={classes.BlogBackIma}>
-                        <img className={classes.BlogBackImage} src={item.BackImage}  />
-                    </div>
-                    <div className={classes.BlogHeadUser}>
-                        <div className={classes.Bloguserima}>
-                            <img src={item.UserImage} className={classes.Blogusrimg}/>
+                <Row>
+                    <div className={classes.BlogCard}>
+                        <div className={classes.BlogBackIma}>
+                            <img className={classes.BlogBackImage} src={item.BackImage} />
                         </div>
-                        <div className={classes.Blogusrinfo} >
-                            <p className={classes.blogusrName}>{item.Name}</p>
-                            <p className={classes.blogusrName}>{item.Title}</p>
+                        <div className={classes.BlogHeadUser}>
+                            <div className={classes.Bloguserima}>
+                                <img src={item.UserImage} className={classes.Blogusrimg} />
+                            </div>
+                            <div className={classes.Blogusrinfo} >
+                                <p className={classes.blogusrName}>{item.Name}</p>
+                                <p className={classes.blogusrName}>{item.Title}</p>
+                            </div>
+                        </div>
+                        <h5 className={classes.Blogusrheading}>
+                            {item.Head}
+                        </h5>
+                        <p className={classes.BlogDesp}>
+                            {item.Description}
+                        </p>
+                        <div className={classes.BlogFooter}>
+                            <p className={classes.BolgDtime}>{item.DateTime}</p>
+                            <div className={classes.BlogRef}>
+                                Exective Eaducation
+                            </div>
                         </div>
                     </div>
-                </div>
-            </Row>
+                </Row>
             </>
         )
     })
-  return (
-    <>
-    <Navbar/>
-    <Container fluid className={classes.MainBlogg}>
-        {BlogsMap}
-    </Container>
-    </>
-  )
+    return (
+        <>
+            <Navbar />
+            <Container fluid className={classes.MainBlogg}>
+                {BlogsMap}
+            </Container>
+        </>
+    )
 }
 
 export default Blogs
--- client/src/Components/BlogsData.jsx
+++ client/src/Components/BlogsData.jsx
@@ -1,59 +1,39 @@
 const BlogsData = [
     {
         Name:"Hassan",
-        Title:"Risk",
+        Title:"Risk Free",
         UserImage:"https://i.pinimg.com/564x/b1/04/44/b10444b33c057da635bb221987230355.jpg",
         BackImage:"https://seefar.org/wp-content/uploads/using-machine-learning.jpg",
-        Head:"CyberSecurity:Mananging "
+        Head:"Markiting Mananger ",
+        Description:"Lorem ipsum dolor esse ad reiciendis voluptates commodi quibusdam molestiae?",
+        DateTime:"25-09-2007"
     },
     {
         Name:"Ahmed",
-        Title:"Risk",
-        UserImage:"https://i.pinimg.com/564x/b1/04/44/b10444b33c057da635bb221987230355.jpg",
-        BackImage:"https://seefar.org/wp-content/uploads/using-machine-learning.jpg",
-        Head:"CyberSecurity:Mananging "
+        Title:"Risk Free",
+        UserImage:"https://www.unigreet.com/wp-content/uploads/2022/01/today-best-whatsapp-dp-2022.jpg",
+        BackImage:"https://cdn.wallpapersafari.com/61/53/G8PFzr.jpg",
+        Head:"Language Master ",
+        Description:"Lorem ipsum dolor esse ad reiciendis voluptates commodi quibusdam molestiae?",
+        DateTime:"15-12-1999"
     },
     {
         Name:"Umer",
-        Title:"Risk",
-        UserImage:"https://i.pinimg.com/564x/b1/04/44/b10444b33c057da635bb221987230355.jpg",
-        BackImage:"https://seefar.org/wp-content/uploads/using-machine-learning.jpg",
-        Head:"CyberSecurity:Mananging "
+        Title:"Risk Free",
+        UserImage:"https://cdn.statusqueen.com/dpimages/thumbnail/dp%20image58-681.jpg",
+        BackImage:"https://i.pinimg.com/736x/a6/d4/e1/a6d4e17a2aa4c201d8116da5b6abd44c--really-cool-wallpapers-colorful-wallpaper.jpg",
+        Head:"Cyber Security Expert ",
+        Description:"Lorem ipsum dolor esse ad reiciendis voluptates commodi quibusdam molestiae?",
+        DateTime:"05-05-2001"
     },
     {
         Name:"Awais",
-        Title:"Risk",
-        UserImage:"https://i.pinimg.com/564x/b1/04/44/b10444b33c057da635bb221987230355.jpg",
-        BackImage:"https://seefar.org/wp-content/uploads/using-machine-learning.jpg",
-        Head:"CyberSecurity:Mananging "
-    },
-    {
-        Name:"Sagheer",
-        Title:"Risk",
-        UserImage:"https://i.pinimg.com/564x/b1/04/44/b10444b33c057da635bb221987230355.jpg",
-        BackImage:"https://seefar.org/wp-content/uploads/using-machine-learning.jpg",
-        Head:"CyberSecurity:Mananging "
-    },
-    {
-        Name:"Saqlain",
-        Title:"Risk",
-        UserImage:"https://i.pinimg.com/564x/b1/04/44/b10444b33c057da635bb221987230355.jpg",
-        BackImage:"https://seefar.org/wp-content/uploads/using-machine-learning.jpg",
-        Head:"CyberSecurity:Mananging "
-    },
-    {
-        Name:"Faizan",
-        Title:"Risk",
-        UserImage:"https://i.pinimg.com/564x/b1/04/44/b10444b33c057da635bb221987230355.jpg",
-        BackImage:"https://seefar.org/wp-content/uploads/using-machine-learning.jpg",
-        Head:"CyberSecurity:Mananging "
-    },
-    {
-        Name:"Gohar",
-        Title:"Risk",
-        UserImage:"https://i.pinimg.com/564x/b1/04/44/b10444b33c057da635bb221987230355.jpg",
-        BackImage:"https://seefar.org/wp-content/uploads/using-machine-learning.jpg",
-        Head:"CyberSecurity:Mananging "
+        Title:"Risk Free",
+        UserImage:"https://1.bp.blogspot.com/-u4tzbKlhVOQ/YAh4yivxV-I/AAAAAAAAkrs/Jpft5HKlkIUXxaboKy85shkczQuNQwe4gCLcBGAsYHQ/s738/whatsapp%2Bdp%2Bimages%2Bfor%2Bboys%2B%25282%2529.jpg",
+        BackImage:"https://wallpaperaccess.com/full/2178327.jpg",
+        Head:"Technical Expert ",
+        Description:"Lorem ipsum dolor esse ad reiciendis voluptates commodi quibusdam molestiae?",
+        DateTime:"03-08-2005"
     },
 ]
 
