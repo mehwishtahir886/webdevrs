dc586666dec6c88b0dd938bfbdd6dbffc07b1d2a
mehwishtahir886 <mehwishtahir886@gmail.com>
2022-06-14 19:16:48 +0500

    login
    navigation

--- client/src/Actions/userActions.js
+++ client/src/Actions/userActions.js
@@ -43,7 +43,7 @@ export const login = (email, password) => async (dispatch) => {
       { email, password },
       config
     )
-
+console.log(data,".....")
     dispatch({
       type: USER_LOGIN_SUCCESS,
       payload: data,
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -13,7 +13,7 @@ import {
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 router.route('/').post(registerUser).get(protect, admin, getUsers)
-// router.post('/login', authUser)
+router.post('/login', authUser)
 // router
 //   .route('/profile')
 //   .get(protect, getUserProfile)
