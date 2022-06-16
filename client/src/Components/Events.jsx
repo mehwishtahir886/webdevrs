11ae52252c4df269798262a6d9940f7cb9e58336
unknown <awaisweb4@gmail.com>
2022-06-16 15:00:06 +0500

    Add Event Blcks

--- client/src/Components/Events.jsx
+++ client/src/Components/Events.jsx
@@ -3,7 +3,8 @@ import classes from "./Events.module.css"
 import Navbar from "./Navbar"
 import Footer from './Footer'
 import web1 from '../videos/web.mp4';
-import { Container, Row } from 'react-bootstrap'
+import Event1 from "../images/Event1.jpg"
+import { Container, Row, Col } from 'react-bootstrap'
 const Events = () => {
     return (
         <>
@@ -29,7 +30,47 @@ const Events = () => {
                     <li>Instructors</li>
                 </div>
                 <Container className={classes.EventsGallery}>
-                        
+                    <Row >
+                        <Col className={classes.Gallery_part1} lg={5}>
+                            <div className={classes.Gallery_part1_img}>
+                                <div className={classes.EventOverlay}></div>
+                                <img className={classes.Eventimg} src={Event1}/>
+                            </div>
+                            <div className={classes.Gallery_part1_content}>
+                                <div className={classes.content_inner}>
+                                    <h6 className={classes.EventDate} >
+                                        29 july,1998
+                                    </h6>
+                                    <h5 className={classes.EventHEading}>
+                                        Seminaars in College
+                                    </h5>
+                                    <p className={classes.EventDescreption}>
+                                        Lorem ipsum dolor sit amet consectetur, dolorum rerum facilis. Molestias ipsum sunt maiores corporis cupiditate delectus nisi earum eaque quo obcaecati.
+                                    </p>
+                                </div>
+                            </div>
+                        </Col>
+                        <Col className={classes.Gallery_part1} lg={5}>
+                            <div className={classes.Gallery_part1_img}>
+                                <div className={classes.EventOverlay}></div>
+                                <img className={classes.Eventimg} src={Event1}/>
+                            </div>
+                            <div className={classes.Gallery_part1_content}>
+                                <div className={classes.content_inner}>
+                                    <h6 className={classes.EventDate} >
+                                        29 july,1998
+                                    </h6>
+                                    <h5 className={classes.EventHEading}>
+                                        Seminaars in College
+                                    </h5>
+                                    <p className={classes.EventDescreption}>
+                                        Lorem ipsum dolor sit amet consectetur, dolorum rerum facilis. Molestias ipsum sunt maiores corporis cupiditate delectus nisi earum eaque quo obcaecati.
+                                    </p>
+                                </div>
+                            </div>
+                        </Col>
+
+                    </Row>
                 </Container>
             </Container>
             <Footer />
