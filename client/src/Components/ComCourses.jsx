c8326b899c333c17e6d3ca70680b7dd238e55067
Hassan Fareed <hassanwd94@gmail.com>
2022-06-16 16:10:09 +0500

    changes

--- client/src/Components/ComCourses.jsx
+++ client/src/Components/ComCourses.jsx
@@ -1,18 +1,35 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
+
+import './ComCourses.css'
+
 import { AiFillCheckCircle } from "react-icons/ai"
-import { Container,Row,Col } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 import classes from './ComCourses.module.css'
-import C1 from '../images/frontend.jfif'
-import C2 from '../images/backend.jfif'
-import C3 from '../images/cloud.jfif'
+import logo from '../images/logo.png'
+import C1 from '../images/frontend.png'
+import C2 from '../images/backend.png'
+import C3 from '../images/cloud.png'
 import C4 from '../images/mobile.png'
-// import C_banner_icon1 from '../images/vector-main-courses/VExtor – 3.png'
-// import C_banner_icon2 from '../images/vector-main-courses/VExtor – 2.png'
-// import C_banner_icon3 from '../images/vector-main-courses/VExtor – 5.png'
-// import C_banner_icon4 from '../images/vector-main-courses/VExtor – 4.png'
+import C_banner_icon1 from '../images/vector-main-courses/VExtor___1.png'
+import C_banner_icon2 from '../images/vector-main-courses/VExtor___2.png'
+
+import Nav from 'react-bootstrap/Nav';
+
+import LanguageImage1 from '../images/VECTOR SLIDEBAR ALL COURSES/HTML.jpg'
+import LanguageImage2 from '../images/VECTOR SLIDEBAR ALL COURSES/CSS.jpg'
+import LanguageImage3 from '../images/VECTOR SLIDEBAR ALL COURSES/Javascript.jpg'
+import LanguageImage4 from '../images/VECTOR SLIDEBAR ALL COURSES/BootStarrp.jpg'
+import LanguageImage5 from '../images/VECTOR SLIDEBAR ALL COURSES/tailment CSS.jpg'
+import LanguageImage6 from '../images/VECTOR SLIDEBAR ALL COURSES/JQuerry.jpg'
+import LanguageImage7 from '../images/VECTOR SLIDEBAR ALL COURSES/GITHUB.jpg'
+import LanguageImage8 from '../images/VECTOR SLIDEBAR ALL COURSES/Redux.jpg'
+
 import Carouselslider from './Carouselslider'
+
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 
 const ComCourses = () => {
   return (
@@ -21,88 +38,155 @@ const ComCourses = () => {
       <Container fluid className={classes.Ccourses_main}>
         <Row>
           <Col className={classes.Ccourses_banner_info}>
-              <p>We've a unique Courses</p>
-              <p>WebDevrs</p>
-              <p>A Web Designing course belongs to the field of Computer Science and IT. It enables students to learn various techniques, tools and programming languages in order to create and maintain web pages. There is an array of courses in this field ranging from certificate and Diploma courses to UG, PG and PGDM programs.</p>
-              <ul>
-                <li><AiFillCheckCircle className={classes.Ccourses_banner_info_icons} /><span>Supporting development efforts from projects.</span></li>
-                <li><AiFillCheckCircle className={classes.Ccourses_banner_info_icons} /><span>Prototyping and building applications and/or websites.</span></li>
-                <li><AiFillCheckCircle className={classes.Ccourses_banner_info_icons} /><span>Designing front end interfaces and user experiences.</span></li>
-              </ul>
+            <p>We've a unique Courses</p>
+            <p>WebDevrs</p>
+            <p>A Web Designing course belongs to the field of Computer Science and IT. It enables students to learn various techniques, tools and programming languages in order to create and maintain web pages. There is an array of courses in this field ranging from certificate and Diploma courses to UG, PG and PGDM programs.</p>
+            <ul>
+              <li><AiFillCheckCircle className={classes.Ccourses_banner_info_icons} /><span>Supporting development efforts from projects.</span></li>
+              <li><AiFillCheckCircle className={classes.Ccourses_banner_info_icons} /><span>Prototyping and building applications and/or websites.</span></li>
+              <li><AiFillCheckCircle className={classes.Ccourses_banner_info_icons} /><span>Designing front end interfaces and user experiences.</span></li>
+              <li><AiFillCheckCircle className={classes.Ccourses_banner_info_icons} /><span>Designing front end interfaces and user experiences.</span></li>
+            </ul>
           </Col>
           <Col className={classes.Ccourses_banner}>
             <div className={classes.Ccourses_banners}>
-              {/* <div className={classes.C_banner__icon_main}><img src={C_banner_icon1} /></div> */}
+              <div className={classes.C_banner__icon_main}><img src={C_banner_icon1} /></div>
               <span>Front End Development</span>
             </div>
             <div className={classes.Ccourses_banners}>
-              {/* <div className={classes.C_banner__icon_main}><img src={C_banner_icon2} /></div> */}
-              <span>Front End Development</span>
+              <div className={classes.C_banner__icon_main}><img src={C_banner_icon2} /></div>
+              <span>Cloud Computing</span>
             </div>
             <div className={classes.Ccourses_banners}>
-              {/* <div className={classes.C_banner__icon_main}><img src={C_banner_icon3} /></div> */}
-              <span>Front End Development</span>
+              <div className={classes.C_banner__icon_main}><img src={C2} /></div>
+              <span>Back End Development</span>
             </div>
             <div className={classes.Ccourses_banners}>
-              {/* <div className={classes.C_banner__icon_main}><img src={C_banner_icon4} /></div> */}
-              <span>Front End Development</span>
+              <div className={classes.C_banner__icon_main}><img src={C4} /></div>
+              <span>Mobile Application</span>
+            </div>
+            <div className={classes.Ccourses_banners_logo}>
+              <div className={classes.C_banner__icon_main}><img src={logo} /></div>
             </div>
           </Col>
-        </Row>
-        <Row>
-          <Col className={classes.Ccourses_items}>
-            <div className={classes.Ccourses_item}>
-              <div className={classes.Ccourses_item_main}>
-                <div className={classes.Ccourses_item_left}>
-                  <h1>Front End Development</h1>
-                  <p>Front-end web development, also known as client-side development is the practice of producing HTML, CSS and JavaScript for a website or Web Application so that a user can see and interact with them directly. The challenge associated with front end development is that the tools and techniques used to create the front end of a website change constantly and so the developer needs to constantly be aware of how the field is developing.</p>
+          <div id='maintabs' className={classes.main_tabs}>
+          {/* <Nav variant="tabs" defaultActiveKey="/home">
+            <Nav.Item>
+              <Nav.Link href="">Active</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link-1">Option 2</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="disabled">
+                Disabled
+              </Nav.Link>
+            </Nav.Item>
+          </Nav> */}
+            <Tabs
+              className={classes.tabs}
+              defaultActiveKey="Front-end-development"
+              id="uncontrolled-tab-example"
+            >
+              <Tab eventKey="Front-end-development" title="Front-end-development">
+                <Row>
+                  <Col className={classes.Ccourses_items}>
+                    <div className={classes.Ccourses_item}>
+                      <div className={classes.Ccourses_item_main}>
+                        <div className={classes.Ccourses_item_left}>
+                          <h1>Front End Development</h1>
+                          <p>Front-end web development, also known as client-side development is the practice of producing HTML, CSS and JavaScript for a website or Web Application so that a user can see and interact with them directly. The challenge associated with front end development is that the tools and techniques used to create the front end of a website change constantly and so the developer needs to constantly be aware of how the field is developing.</p>
+                        </div>
+                        <div className={classes.Ccourses_item_image}><img src={C1} /></div>
+                      </div>
+                      <div className={classes.Ccourses_item_projects}>
+                        <div>
+                          <div><img src={LanguageImage1} /></div>
+                          <span>HTML</span>
+                        </div>
+                        <div>
+                          <div><img src={LanguageImage2} /></div>
+                          <span>CSS</span>
+                        </div>
+                        <div>
+                          <div><img src={LanguageImage3} /></div>
+                          <span>Javascript</span>
+                        </div>
+                        <div>
+                          <div><img src={LanguageImage4} /></div>
+                          <span>Bootstrap</span>
+                        </div>
+                        <div>
+                          <div><img src={LanguageImage5} /></div>
+                          <span>Tailwind css</span>
+                        </div>
+                        <div>
+                          <div><img src={LanguageImage6} /></div>
+                          <span>Jquery</span>
+                        </div>
+                        <div>
+                          <div><img src={LanguageImage7} /></div>
+                          <span>Git Hub</span>
+                        </div>
+                        <div>
+                          <div><img src={LanguageImage8} /></div>
+                          <span>Redux</span>
+                        </div>
+                      </div>
+                      <div className={classes.Ccourses_item_carousel}>
+                        <Carouselslider />
+                      </div>
+                    </div>
+                  </Col>
+                </Row>
+              </Tab>
+              <Tab eventKey="Back-end-development" title="Back-end-development">
+                <div className={classes.Ccourses_item}>
+                  <div className={classes.Ccourses_item_main}>
+                    <div className={classes.Ccourses_item_left}>
+                      <h1>Back End Development</h1>
+                      <p>Backend Development is also known as server-side development. It is everything that the users don't see and contains behind-the-scenes activities that occur when performing any action on a website. It focuses primarily on databases, backend logic, APIs, and Servers.</p>
+                    </div>
+                    <div className={classes.Ccourses_item_image}><img src={C2} /></div>
+                  </div>
+                  <div className={classes.Ccourses_item_carousel}>
+                    <Carouselslider />
+                  </div>
                 </div>
-                <div className={classes.Ccourses_item_image}><img src={C1} /></div>
-              </div>
-              <div className={classes.Ccourses_item_carousel}>
-                <Carouselslider />
-              </div>
-            </div>
-            <div className={classes.Ccourses_item}>
-              <div className={classes.Ccourses_item_main}>
-                  <div className={classes.Ccourses_item_left}>
-                  <h1>Back End Development</h1>
-                  <p>Backend Development is also known as server-side development. It is everything that the users don't see and contains behind-the-scenes activities that occur when performing any action on a website. It focuses primarily on databases, backend logic, APIs, and Servers.</p>
+              </Tab>
+              <Tab eventKey="Cloud-computing" title="Cloud-computing">
+                <div className={classes.Ccourses_item}>
+                  <div className={classes.Ccourses_item_main}>
+                    <div className={classes.Ccourses_item_left}>
+                      <h1>Cloud Computing</h1>
+                      <p>Simply put, cloud computing means storing and accessing data over the internet instead of a hard disk. It is defined as a service that provides users to work over the internet. Users can access data on the Cloud and must provide a user ID and password to gain entry, for security reasons.</p>
+                    </div>
+                    <div className={classes.Ccourses_item_image}><img src={C3} /></div>
+                  </div>
+                  <div className={classes.Ccourses_item_carousel}>
+                    <Carouselslider />
+                  </div>
                 </div>
-                <div className={classes.Ccourses_item_image}><img src={C2} /></div>
-              </div>
-              <div className={classes.Ccourses_item_carousel}>
-                <Carouselslider />
-              </div>
-            </div>
-            <div className={classes.Ccourses_item}>
-              <div className={classes.Ccourses_item_main}>
-                  <div className={classes.Ccourses_item_left}>
-                  <h1>Cloud Computing</h1>
-                  <p>Simply put, cloud computing means storing and accessing data over the internet instead of a hard disk. It is defined as a service that provides users to work over the internet. Users can access data on the Cloud and must provide a user ID and password to gain entry, for security reasons.</p>
+              </Tab>
+              <Tab eventKey="Mobile-application" title="Mobile-application">
+                <div className={classes.Ccourses_item}>
+                  <div className={classes.Ccourses_item_main}>
+                    <div className={classes.Ccourses_item_left}>
+                      <h1>Mobile Application</h1>
+                      <p>A mobile application, most commonly referred to as an app, is a type of application software designed to run on a mobile device, such as a smartphone or tablet computer. Mobile applications frequently serve to provide users with similar services to those accessed on PCs.</p>
+                    </div>
+                    <div className={classes.Ccourses_item_image}><img src={C4} /></div>
+                  </div>
+                  <div className={classes.Ccourses_item_carousel}>
+                    <Carouselslider />
+                  </div>
                 </div>
-                <div className={classes.Ccourses_item_image}><img src={C3} /></div>
-              </div>
-              <div className={classes.Ccourses_item_carousel}>
-                <Carouselslider />
-              </div>
-            </div>
-            <div className={classes.Ccourses_item}>
-              <div className={classes.Ccourses_item_main}>
-                  <div className={classes.Ccourses_item_left}>
-                  <h1>Mobile Application</h1>
-                  <p>A mobile application, most commonly referred to as an app, is a type of application software designed to run on a mobile device, such as a smartphone or tablet computer. Mobile applications frequently serve to provide users with similar services to those accessed on PCs.</p>
-                </div>
-                <div className={classes.Ccourses_item_image}><img src={C4} /></div>
-              </div>
-              <div className={classes.Ccourses_item_carousel}>
-                <Carouselslider />
-              </div>
-            </div>
-          </Col>
+              </Tab>
+            </Tabs>
+          </div>
         </Row>
       </Container>
-      <Footer /> 
+      <Footer />
     </>
   )
 }
