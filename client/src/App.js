059cd51d80da04eccb03c9f89ccc7849ac39ccbc
Hassan Fareed <hassanwd94@gmail.com>
2022-06-20 12:45:55 +0500

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -10,11 +10,7 @@ import Forget from './Components/Forget'
 import Blogs from './Components/Blogs'
 import Events from './Components/Events'
 import Reviews from './Components/Reviews'
-<<<<<<< HEAD
-import About from './Components/About'
-=======
 import ProjectDetail from './Components/ProjectDetail'
->>>>>>> 4c51c2e16d4666369059da55ee8a5a35d0042511
 
 const App = () => {
   return (
@@ -23,12 +19,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
-<<<<<<< HEAD
-        <Route path="/about" element={<About />} />
-        <Route path="//ContactPage" element={<ContactsPage />} />
-=======
         <Route path="/ContactPage" element={<ContactsPage />} />
->>>>>>> 4c51c2e16d4666369059da55ee8a5a35d0042511
         <Route path='/login' element={<Login />} />
         <Route path='/Signup' element={<SignUp />} />
         <Route path='/courses' element={<ComCourses />} />
