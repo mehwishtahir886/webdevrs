c7955c9bb14a3c8387c5b9833213a64069f9e821
unknown <awaisweb4@gmail.com>
2022-06-21 15:39:14 +0500

    Add DashBoard

---
+++ client/src/Dashboard/DashBoard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from "./DashBoard.module.css"
+
+const DashBoard = () => {
+  return (
+    <div>
+      This is Dash Board    
+    </div>
+  )
+}
+
+export default DashBoard
---
+++ client/src/Dashboard/TheNav.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./TheNav.module.css"
+
+const TheNav = () => {
+  return (
+    <div>TheNav</div>
+  )
+}
+
+export default TheNav
