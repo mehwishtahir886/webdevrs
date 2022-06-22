0e9904e485e577da3b63453063f6d0edf0699636
unknown <awaisweb4@gmail.com>
2022-06-22 12:00:49 +0500

    Add Data

--- client/src/Dashboard/DashBoard.jsx
+++ client/src/Dashboard/DashBoard.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import { Container, Row } from 'react-bootstrap'
 import classes from "./DashBoard.module.css"
+import SideBar from './SideBar'
+import TheNav from './TheNav'
 
 const DashBoard = () => {
-  return (
-    <div>
-      This is Dash Board    
-    </div>
-  )
+    return (
+        <>
+            <SideBar />
+            <Container fluid className={classes.MainDashBoard}>
+                <TheNav/>
+                Admin Data there we add
+            </Container>
+        </>
+    )
 }
 
 export default DashBoard
--- client/src/Dashboard/TheNav.jsx
+++ client/src/Dashboard/TheNav.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-import "./TheNav.module.css"
+import classes from "./TheNav.module.css"
+import { BsSearch } from "react-icons/bs"
 
 const TheNav = () => {
   return (
-    <div>TheNav</div>
+    <>
+    <nav className={classes.DashBoardNav}>
+        <div className={classes.DashSearch}>
+          <div className={classes.SearchMain}>
+              <input className={classes.SearchInput} placeholder="Search..........." />
+              <BsSearch className={classes.SearchIcon}/>
+          </div>
+        </div>
+        <div className={classes.DashAdmin}></div>
+    </nav>
+    </>
   )
 }
 
