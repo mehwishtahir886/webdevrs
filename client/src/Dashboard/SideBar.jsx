18755086ba31846f4d8c9c0b9551916decae44b6
unknown <awaisweb4@gmail.com>
2022-06-28 17:18:46 +0500

    Add

--- client/src/Dashboard/SideBar.jsx
+++ client/src/Dashboard/SideBar.jsx
@@ -6,14 +6,13 @@ const SideBar = () => {
     <div className={classes.MainSide}>
       <div className={classes.DashLogo}><h3>Admin Panel</h3></div>
       <h5 className={classes.DashSideHead}>DashBoard</h5>
-      <li className={classes.liActive}>All</li>
-      <li>Brand Startergy</li>
-      <li>Brand Support</li>
-      <li>Comminuactions</li>
-      <li>Culture</li>
-      <li>LeaderShip</li>
-      <li>News and Updates</li>
-      <li>Process</li>
+      <li>Students</li>
+      <li>Projects</li>
+      <li>Accounts</li>
+      <li>Upload Project</li>
+      <li>Admissions</li>
+      <li>Employers</li>
+      <li>Upload Blogs</li>
       <li>Visual Identity</li>
       <li>In Conservation</li>
     </div>
